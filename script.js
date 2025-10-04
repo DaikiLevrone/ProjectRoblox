1553e173fc53d6efedce266a349127e6295b1738
Fabricio <149639515+DaikiLevrone@users.noreply.github.com>
2025-10-04 17:28:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('getKeyBtn').addEventListener('click', function () {
-  window.location.href = 'https://www.roblox.com/share?code=d937e84c3b26394c9d38e756537bfa57&type=Server'; // Cambia este link cuando tengas el real
+  window.location.href = 'https://www.roblox.com/share?code=40b9b25727665742a98ad9e691c7758f&type=Server'; // Cambia este link cuando tengas el real
 });
